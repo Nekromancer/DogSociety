a728d100d9f3ddca929d0eeaace2ab2d75eacd09
Nekromancer <jakub.nekro@gmail.com>
2016-01-04 00:33:25 +0100

    Added Kotlin Android Extensions

--- app/src/main/java/pl/ownvision/dogsociety/LoginActivity.kt
+++ app/src/main/java/pl/ownvision/dogsociety/LoginActivity.kt
@@ -1,24 +1,21 @@
 package pl.ownvision.dogsociety
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import com.digits.sdk.android.DigitsAuthButton;
+
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.content_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-        val digitsButton = findViewById(R.id.auth_button) as DigitsAuthButton
         val callback = (application as MainApplication).getDigitsAuthCallback()
-        digitsButton.setCallback(callback)
+        auth_button.setCallback(callback)
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton
         fab.setOnClickListener {
             throw RuntimeException("test")
         }
